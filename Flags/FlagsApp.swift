a8f716a2103ea2098d07cec4b8e1368f383541d9
wuhibm <wuhib.mezemir@gmail.com>
2023-08-11 17:12:33 +0300

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Wuhib Mezemir on 2023-08-11.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
